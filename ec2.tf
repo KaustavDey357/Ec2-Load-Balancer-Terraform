2859db5c344b25e10c1e554952f3b99ac983dd77
kaustav <75007246+KaustavDey357@users.noreply.github.com>
2025-05-14 14:09:54 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,4 +3,11 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.allow_http.id]
+  user_data = <-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y nginx
+              echo "Hello from Terraform EC2" > /var/www/html/index.html
+  EOF
 }
+# this would print "Hello from Terraform EC2" when we open the dns link
