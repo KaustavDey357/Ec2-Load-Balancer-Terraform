97b58f6a215a8d59bad42ea8171b93b681b7135a
kaustav <75007246+KaustavDey357@users.noreply.github.com>
2025-05-14 13:26:39 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -11,6 +11,15 @@ resource "aws_lb_target_group" "web_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener" "web_listener" {
